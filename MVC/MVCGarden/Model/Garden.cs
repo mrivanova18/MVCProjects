c2fb70a8807fefcfe46f0d2b24078392841e61aa
mrivanova18 <m.r.ivanova2019@pmg-pernik.com>
2023-02-13 11:18:15 +0200

    upload project

---
+++ MVC/MVCGarden/Model/Garden.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MVCGarden.Model
+{
+    public class Garden
+    {
+        public Garden(decimal priceVegitables, decimal priceFruits, int kgVegitable, int kgFruits)
+        {
+            PriceVegitables = priceVegitables;
+            PriceFruits = priceFruits;
+            KgVegitable = kgVegitable;
+            KgFruits = kgFruits;
+        }
+
+        public decimal PriceVegitables { get; set; }
+        public decimal PriceFruits { get; set; }
+        public int KgVegitable { get; set; }
+        public int KgFruits { get; set; }
+        public decimal CalculateSum()
+        {
+            return 1.94m * (this.KgFruits * this.PriceFruits + this.KgVegitable * this.PriceVegitables);
+        }
+    }
+}
